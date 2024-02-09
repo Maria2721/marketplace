c7cf89b35cdde3065f8244945a1be5042d759660
Maria2721 <mashaivvas@gmail.com>
2024-02-10 02:28:50 +0300

    add settings for deployment

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { store } from "./store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename="/marketplace">
       <App />
     </BrowserRouter>
   </Provider>
